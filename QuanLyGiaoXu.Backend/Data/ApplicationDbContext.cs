49ac5c8e0f43a9034ff99db647a801e37576f8c9
NguyenTai26072004 <anhtai26072004@gmail.com>
2025-10-12 01:24:33 +0700

    feat(infra): Configure DbContext and application services

---
+++ QuanLyGiaoXu.Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using QuanLyGiaoXu.Backend.Entities;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace QuanLyGiaoXu.Backend.Data 
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<UserClassAssignment> UserClassAssignments { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Score> Scores { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserClassAssignment>().HasKey(uca => new { uca.UserId, uca.ClassId });
+
+            builder.Entity<UserClassAssignment>().HasOne(uca => uca.User)
+                .WithMany(u => u.UserClassAssignments).HasForeignKey(uca => uca.UserId);
+
+            builder.Entity<UserClassAssignment>().HasOne(uca => uca.Class)
+                .WithMany(c => c.UserClassAssignments).HasForeignKey(uca => uca.ClassId);
+
+            builder.Entity<Student>().HasIndex(s => s.StudentCode).IsUnique();
+
+            builder.Entity<Grade>()
+                .HasIndex(g => g.Name)
+                .IsUnique();
+        }
+    }
+}
+
---
+++ QuanLyGiaoXu.Backend/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using QuanLyGiaoXu.Backend.Data;
+using QuanLyGiaoXu.Backend.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 1. Thêm các dịch vụ vào container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// 2. Cấu hình DbContext với SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// 3. Cấu hình Identity
+builder.Services.AddIdentityCore<User>(options => {
+    options.Password.RequireNonAlphanumeric = false;
+    
+})
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
